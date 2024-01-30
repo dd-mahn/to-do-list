eece30a02e6bc35e854ead4856f72dcfda3219f1
godsadeser <ducmanh0801@gmail.com>
2024-01-30 16:08:02 +0700

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/static/main.js'
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname,'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '2Doo',
+            template: './src/templates/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 
+                {loader:'css-loader', options: {minimize: true}}]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: 'images/[name].[ext]',
+                    },
+                  },
+                ],
+              },
+        ]
+    }
+
+}
