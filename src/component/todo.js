e77c530c448a207af32728d32bb5d3ac13969bbf
godsadeser <ducmanh0801@gmail.com>
2024-02-03 14:37:50 +0700

    Initial structure

---
+++ src/component/todo.js
@@ -0,0 +1,33 @@
+import {format} from 'date-fns'
+
+export default function todo(){
+    let title, description, dueDate, priority
+    const startDate = format(new Date(), 'MM/dd/yyyy')
+    let status = false
+
+    const changeValue = (tit,des,due,pri) => {
+        title = tit
+        description = des
+        dueDate = format(due, 'MM/dd/yyyy')
+        priority = pri
+    }
+
+    const changeStatus = () => {
+        status === false ? true : false
+    }
+
+    const getValue = () => {
+        title,
+        description,
+        startDate,
+        dueDate,
+        priority,
+        status 
+    }
+
+    return{
+        changeValue,
+        changeStatus,
+        getValue
+    }
+}
