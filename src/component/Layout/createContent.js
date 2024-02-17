a2a1de0699bf7c309ef5dcd6b9e1404c2cf040fa
godsadeser <ducmanh0801@gmail.com>
2024-02-18 02:07:38 +0700

    finish construction

--- src/component/Layout/createContent.js
+++ src/component/Layout/createContent.js
@@ -1,3 +1,19 @@
-export default function createContent(title){
+export default function createContent(prj){
 
+    const title = prj.getValue().title
+
+    const contentDiv = document.createElement('div')
+    content.classList.add('content')
+
+    const titleH1 = document.createElement('h1')
+    titleDiv.innerHTML=title
+    titleDiv.classList.add('content__title')
+
+    const itemDiv = document.createElement('div')
+    itemDiv.classList.add('content__items')
+
+    contentDiv.appendChild(titleH1)
+    contentDiv.appendChild(itemDiv)
+
+    return contentDiv
 }
