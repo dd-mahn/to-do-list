32ed1d0cac52e7e2e0d4a995b84be11d86f1e585
godsadeser <ducmanh0801@gmail.com>
2024-02-18 00:24:26 +0700

    fix hidden menu icon

--- src/component/Layout/createHeader.js
+++ src/component/Layout/createHeader.js
@@ -3,7 +3,10 @@ export default function createHeader(){
 
     const menuBtn = document.createElement('div')
     menuBtn.classList.add('menu__btn','icon__big')
-    menuBtn.innerHTML = `<i class="ri-menu-line></i>`
+    // menuBtn.innerHTML = `<i class="ri-menu-line></i>`
+    const menuIcon = document.createElement('i')
+    menuIcon.classList.add('ri-menu-line')
+    menuBtn.appendChild(menuIcon)
 
     const logo = document.createElement('div')
     logo.classList.add('logo')
@@ -12,7 +15,7 @@ export default function createHeader(){
         <span>Tasko</span>`
 
     const addBtn = document.createElement('div')
-    addBtn.classList.add('add__btn icon__big')
+    addBtn.classList.add('add__btn','icon__big')
     addBtn.innerHTML = `<i class="ri-add-line"></i>`
 
     header.appendChild(menuBtn)
