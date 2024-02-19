1c71efd7b8e820be0f6d5250693223681aa26a3b
godsadeser <ducmanh0801@gmail.com>
2024-02-20 01:06:15 +0700

    add items generator and clean code

--- src/component/Layout/createContent.js
+++ src/component/Layout/createContent.js
@@ -1,19 +1,38 @@
-export default function createContent(prj){
+import createTodo from "./createTodo";
+import createNote from "./createNote";
+import todo from "../todo";
+import note from "../note";
+import appendChildren from "../../utils/appendChildren";
+import createDivWithClass from "../../utils/createDivWithClass";
 
-    const title = prj.getValue().title
+export default function createContent(prj) {
+    const title = prj.getValue().title;
 
-    const contentDiv = document.createElement('div')
-    content.classList.add('content')
+    const contentDiv = createDivWithClass('content');
 
-    const titleH1 = document.createElement('h1')
-    titleDiv.innerHTML=title
-    titleDiv.classList.add('content__title')
+    const titleDiv = createDivWithClass('content__title');
+    const titleH1 = document.createElement('h1');
+    titleH1.innerHTML = title;
 
-    const itemDiv = document.createElement('div')
-    itemDiv.classList.add('content__items')
+    const titleIcon = document.createElement('i');
+    titleIcon.classList.add('ri-information-2-fill');
 
-    contentDiv.appendChild(titleH1)
-    contentDiv.appendChild(itemDiv)
+    const itemDiv = createDivWithClass('content__items');
 
-    return contentDiv
+    const items = prj.getAllItem();
+    items.forEach(item => {
+        const itemType = item.getType();
+        if (itemType === 'note') {
+            itemDiv.appendChild(createNote(item));
+        } else if (itemType === 'todo') {
+            itemDiv.appendChild(createTodo(item));
+        }
+    });
+
+    appendChildren(titleDiv, [titleH1, titleIcon]);
+    appendChildren(contentDiv, [titleDiv, itemDiv]);
+
+    return contentDiv;
 }
+
+
