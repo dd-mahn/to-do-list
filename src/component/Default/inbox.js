ea3ba712666a38a86e1ed3f4411f650d3ca6a394
godsadeser <ducmanh0801@gmail.com>
2024-02-20 01:04:12 +0700

    add default items

--- src/component/Default/inbox.js
+++ src/component/Default/inbox.js
@@ -1,8 +1,24 @@
-import project from "../../static/project";
+import project from "../project";
+import todo from "../todo"
+import note from "../note"
+
 
 const inbox = () => {
     const inboxObj = Object.create(project())
-    
+    inboxObj.changeValue('Inbox', 'Where you store all your new stuffs')
+
+    const todo1 = todo()
+    const todo1Date = new Date()
+    todo1Date.setFullYear(todo1Date.getFullYear() + 1)
+    todo1.changeValue('Create your first project!', 'Click the add button on our menu', todo1Date, 'high')
+
+    const note1 = note()
+    note1.changeValue('Today is a wonderful day!', 'I love you <3')
+
+    inboxObj.addItem(todo1)
+    inboxObj.addItem(note1)
+
+    return inboxObj
 }
 
 export default inbox
