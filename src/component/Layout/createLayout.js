51c074add57f451ea93c66abea3e3a7d9b2ee65e
godsadeser <ducmanh0801@gmail.com>
2024-02-20 01:20:14 +0700

    add inbox section, clean code

--- src/component/Layout/createLayout.js
+++ src/component/Layout/createLayout.js
@@ -1,14 +1,18 @@
 import header from './createHeader'
 import navBar from './createNavigation'
 import menu from './createMenu'
+import createContent from './createContent'
+import inbox from '../Default/inbox'
+import appendChildren from '../../utils/appendChildren'
 
 export default function createLayout(){
     const body = document.querySelector('body')
+
     const container = document.createElement('section')
     container.classList.add('container')
 
-    body.appendChild(header())
-    body.appendChild(container)
-    container.appendChild(navBar())
-    container.appendChild(menu())
+    const content = createContent(inbox())
+
+    appendChildren(body, [header(), container])
+    appendChildren(container, [navBar(), menu(), content])
 }
