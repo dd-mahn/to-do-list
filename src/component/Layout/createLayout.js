d5fc8ac7378003df686578e75690ae2b8fcbb7da
godsadeser <ducmanh0801@gmail.com>
2024-02-19 17:22:52 +0700

    change file name

---
+++ src/component/Layout/createLayout.js
@@ -0,0 +1,14 @@
+import header from './createHeader'
+import navBar from './createNavigation'
+import menu from './createMenu'
+
+export default function createLayout(){
+    const body = document.querySelector('body')
+    const container = document.createElement('section')
+    container.classList.add('container')
+
+    body.appendChild(header())
+    body.appendChild(container)
+    container.appendChild(navBar())
+    container.appendChild(menu())
+}
