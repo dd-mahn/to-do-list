533b603bd6b09aed3ae338b1d2c33ec5049f812e
godsadeser <ducmanh0801@gmail.com>
2024-02-21 00:30:42 +0700

    rename folder

--- src/component/Default Project/today.js
+++ src/component/Default Project/today.js
@@ -1,8 +1,10 @@
 import project from "../../static/project";
+import format from 'date-fns'
+
 
 const today = () => {
     const todayObj = Object.create(project())
-    
+    const curDate = format(new Date())
 }
 
 export default today
