a69c7d92951642fd72db3006d16a3900b2a5c0a0
godsadeser <ducmanh0801@gmail.com>
2024-02-24 00:58:38 +0700

    Initial

---
+++ src/utils/switching/changeDetail.js
@@ -0,0 +1,10 @@
+import createDetail from "../../component/Layout/createDetail"
+
+export default function changeDetail(obj){
+    const container = document.querySelector('.container')
+    const oldDetail = container.querySelector('.detail')
+    const newDetail = createDetail(obj)
+
+    container.removeChild(oldDetail)
+    container.appendChild(newDetail)
+}
