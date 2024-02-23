75a35b5bd41bbc8208df349d946eef633b887e18
godsadeser <ducmanh0801@gmail.com>
2024-02-24 01:12:53 +0700

    Move to new folder

---
+++ src/utils/ui-handlers/contentHandler.js
@@ -0,0 +1,23 @@
+import closeThis from "../common/closeThis"
+import openThis from "../common/openThis"
+
+export default function contentHandler(){
+    const titleDiv = document.querySelector('.content__title')
+    const detailBtn = titleDiv.querySelector('i')
+
+    detailBtn.addEventListener('click', () => {
+        detailController()
+    })
+}
+
+function detailController(){
+    const content = document.querySelector('.content')
+    const detail = document.querySelector('detail')
+    if(detail.classList.contains('d-off')){
+        openThis(detail)
+        content.classList.add('show__detail')
+    }else{
+        closeThis(detail)
+        content.classList.add('show__detail')
+    }
+}
