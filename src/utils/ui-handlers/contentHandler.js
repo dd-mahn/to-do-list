b2bd78b0847aaa8270c44fae504202353fed6700
godsadeser <ducmanh0801@gmail.com>
2024-02-26 17:04:24 +0700

    Complete open and close detail logic

--- src/utils/ui-handlers/contentHandler.js
+++ src/utils/ui-handlers/contentHandler.js
@@ -1,23 +1,31 @@
 import closeThis from "../common/closeThis"
 import openThis from "../common/openThis"
+import {getCurrentState} from '../state'
+import changeDetail from "./changeDetail"
+import { detailOpen, detailHandler } from "./detailHandler"
 
 export default function contentHandler(){
     const titleDiv = document.querySelector('.content__title')
     const detailBtn = titleDiv.querySelector('i')
+    const contentItems = document.querySelectorAll('.content__item')
+    const currentProject = getCurrentState()
 
     detailBtn.addEventListener('click', () => {
-        detailController()
+        changeDetail(currentProject)
+        detailOpen()
+        detailHandler()
+        console.log('detail btn clicked')
+    })
+
+    contentItems.forEach((item,index) => {
+        const title = item.querySelector('.title')
+        title.addEventListener('click', () => {
+            console.log('item clicked')
+            changeDetail(currentProject.getItem(index))
+            detailOpen()
+            detailHandler()
+        })
     })
 }
 
-function detailController(){
-    const content = document.querySelector('.content')
-    const detail = document.querySelector('.detail')
-    if(detail.classList.contains('d-off')){
-        openThis(detail)
-        content.classList.add('show__detail')
-    }else{
-        closeThis(detail)
-        content.classList.add('show__detail')
-    }
-}
+
