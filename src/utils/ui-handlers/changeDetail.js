1b8018b1a8d52d870a62e5e773b7fc588f009f3a
godsadeser <ducmanh0801@gmail.com>
2024-02-26 17:04:45 +0700

    Initial

--- src/utils/ui-handlers/changeDetail.js
+++ src/utils/ui-handlers/changeDetail.js
@@ -4,7 +4,7 @@ export default function changeDetail(obj){
     const container = document.querySelector('.container')
     const oldDetail = container.querySelector('.detail')
     const newDetail = createDetail(obj)
-
+    
     container.removeChild(oldDetail)
     container.appendChild(newDetail)
 }
