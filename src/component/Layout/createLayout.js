bfdf50ae274a89197a0ccd7d392d55574b2733b0
godsadeser <ducmanh0801@gmail.com>
2024-02-27 16:46:45 +0700

    add dialogs to layout

--- src/component/Layout/createLayout.js
+++ src/component/Layout/createLayout.js
@@ -3,6 +3,7 @@ import navBar from './createNavigation'
 import menu from './createMenu'
 import createContent from './createContent'
 import createDetail from './createDetail'
+import {createProjectDialog, createItemDialog, createConfirmDialog} from './createDialogs'
 import appendChildren from '../../utils/common/appendChildren'
 
 export default function createLayout(obj){
@@ -13,7 +14,10 @@ export default function createLayout(obj){
 
     const content = createContent(obj)
     const detail = createDetail(obj)
+    const projectDialog = createProjectDialog()
+    const itemDialog = createItemDialog()
+    const confirmDialog = createConfirmDialog()
 
     appendChildren(body, [header(), container])
-    appendChildren(container, [navBar(), menu(), content, detail])
+    appendChildren(container, [navBar(), menu(), content, detail, projectDialog, itemDialog, confirmDialog])
 }
