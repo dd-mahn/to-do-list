44dc98425f5979e7a7c62a9d2188572aea17f130
godsadeser <ducmanh0801@gmail.com>
2024-02-27 15:50:51 +0700

    clean code

--- src/utils/ui-handlers/contentHandler.js
+++ src/utils/ui-handlers/contentHandler.js
@@ -1,8 +1,6 @@
-import closeThis from "../common/closeThis"
-import openThis from "../common/openThis"
 import {getCurrentState} from '../state'
 import changeDetail from "./changeDetail"
-import { detailOpen, detailHandler } from "./detailHandler"
+import { openDetail, detailHandler } from "./detailHandler"
 
 export default function contentHandler(){
     const titleDiv = document.querySelector('.content__title')
@@ -12,17 +10,15 @@ export default function contentHandler(){
 
     detailBtn.addEventListener('click', () => {
         changeDetail(currentProject)
-        detailOpen()
+        openDetail()
         detailHandler()
-        console.log('detail btn clicked')
     })
 
     contentItems.forEach((item,index) => {
         const title = item.querySelector('.title')
         title.addEventListener('click', () => {
-            console.log('item clicked')
             changeDetail(currentProject.getItem(index))
-            detailOpen()
+            openDetail()
             detailHandler()
         })
     })
