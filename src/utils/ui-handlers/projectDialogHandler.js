0822df5c90fb7c368ca2fcefa19c351d5824a93d
godsadeser <ducmanh0801@gmail.com>
2024-02-27 17:08:03 +0700

    Initial declaration

--- src/utils/ui-handlers/projectDialogHandler.js
+++ src/utils/ui-handlers/projectDialogHandler.js
@@ -1,6 +1,6 @@
 export default function projectDialogHandler(){
     const projectDialog = document.getElementById('project__add-dialog')
-    const nameInput = projectDialog.querySelector('.project__name-input')
+    const nameInput = projectDialog.querySelector('.project__title-input')
     const desInput = projectDialog.querySelector('.project__des-input')
     const addBtn = projectDialog.querySelector('.add__btn')
     const closeBtn = projectDialog.querySelector('.close__btn')
