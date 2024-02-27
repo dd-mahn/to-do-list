267a4fca78a0d489162252b8bd4e2648f62a2088
godsadeser <ducmanh0801@gmail.com>
2024-02-28 00:44:25 +0700

    finish add project function

--- src/utils/ui-handlers/projectDialogHandler.js
+++ src/utils/ui-handlers/projectDialogHandler.js
@@ -1,8 +1,29 @@
+import project from "../../component/project"
+import projectContainerObj from "../../component/projectContainer"
+import renderLayout from "../render"
+
 export default function projectDialogHandler(){
     const projectDialog = document.getElementById('project__add-dialog')
-    const nameInput = projectDialog.querySelector('.project__title-input')
-    const desInput = projectDialog.querySelector('.project__des-input')
-    const addBtn = projectDialog.querySelector('.add__btn')
-    const closeBtn = projectDialog.querySelector('.close__btn')
+    const projectForm = document.getElementById('project__add-form')
+    const nameInput = projectForm.querySelector('.project__title-input')
+    const desInput = projectForm.querySelector('.project__des-input')
+    const addBtn = projectForm.querySelector('.add__btn')
+    const closeBtn = projectForm.querySelector('.close__btn')
 
+    closeBtn.addEventListener('click', () => {
+        // projectForm.removeAttribute("novalidate")
+        projectDialog.close("canceled")
+        // projectForm.setAttribute("novalidate","true")
+    })
+
+    addBtn.addEventListener('click', () => {
+        if(nameInput.value !== '' && desInput.value !== ''){
+            const newProject = project()
+            newProject.changeValue(nameInput.value, desInput.value)
+            projectContainerObj.addProject(newProject)
+            renderLayout()
+        }else{
+            console.log('Empty input')
+        }
+    })
 }
