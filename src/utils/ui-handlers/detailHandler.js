4bf1b4f363bf8f468bf86c86c46cc0a4fd743344
godsadeser <ducmanh0801@gmail.com>
2024-02-27 15:51:02 +0700

    clean code

--- src/utils/ui-handlers/detailHandler.js
+++ src/utils/ui-handlers/detailHandler.js
@@ -9,20 +9,22 @@ export function detailHandler(){
 
 
     closeBtn.addEventListener('click', () => {
-        console.log('close button clicked')
         closeThis(detail)
         content.classList.remove('show__detail')
+    })
+
+    deleteBtn.addEventListener('click', () => {
+
     })
     
 }
 
-export function detailOpen(){
+export function openDetail(){
     const content = document.querySelector('.content')
     const detail = document.querySelector('.detail')
     if(detail.classList.contains('d-off')){
         openThis(detail)
         content.classList.add('show__detail')
-        console.log('detail is opened')
         detailHandler()
     }
 }
