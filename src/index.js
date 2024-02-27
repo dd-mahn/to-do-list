fb39dd08ba9c285590622d41d15a184b0b535b3a
godsadeser <ducmanh0801@gmail.com>
2024-02-27 15:51:27 +0700

    minimize root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style/style.css'
-import { setCurrentState } from './utils/state'
 import renderLayout from './utils/render'
-import inbox from './component/Default Project/inbox'
 
 function ScreenController(){
-    setCurrentState(inbox())
     renderLayout()
 }
 
