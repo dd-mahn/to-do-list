87a27af8bd4df9534dd5d8a2706faf03b924c496
godsadeser <ducmanh0801@gmail.com>
2024-02-29 00:11:19 +0700

    add startDate to default items

--- src/component/Default Project/inbox.js
+++ src/component/Default Project/inbox.js
@@ -2,8 +2,9 @@ import project from "../project";
 import todo from "../todo"
 import note from "../note"
 
-const todo1Date = new Date()
-todo1Date.setFullYear(todo1Date.getFullYear() + 1)
+const todo1StartDate = new Date()
+const todo1DueDate = new Date()
+todo1DueDate.setFullYear(todo1DueDate.getFullYear() + 1)
 
 const inbox = () => {
     const inboxObj = Object.create(project())
@@ -11,10 +12,10 @@ const inbox = () => {
 
     const todo1 = todo()
     
-    todo1.changeValue('Create your first project!', 'Click the add button on our menu', todo1Date, 'high')
+    todo1.changeValue('Create your first project!', 'Click the add button on our menu', todo1StartDate, todo1DueDate, 'high')
 
     const note1 = note()
-    note1.changeValue('Today is a wonderful day!', 'I love you <3')
+    note1.changeValue('Today is a wonderful day!', 'Do something interesting today!')
 
     inboxObj.addItem(todo1)
     inboxObj.addItem(note1)
