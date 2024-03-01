8f4b034b26224490a1a2237777c012b62a04df14
godsadeser <ducmanh0801@gmail.com>
2024-03-01 16:25:20 +0700

    Remove showDetail class usage

--- src/utils/ui-handlers/detailHandler.js
+++ src/utils/ui-handlers/detailHandler.js
@@ -5,12 +5,10 @@ export function detailHandler(){
     const detail = document.querySelector('.detail')
     const closeBtn = detail.querySelector('.close__btn')
     const deleteBtn = detail.querySelector('.delete__btn')
-    const content = document.querySelector('.content')
 
 
     closeBtn.addEventListener('click', () => {
         closeThis(detail)
-        content.classList.remove('show__detail')
     })
 
     deleteBtn.addEventListener('click', () => {
@@ -20,11 +18,9 @@ export function detailHandler(){
 }
 
 export function openDetail(){
-    const content = document.querySelector('.content')
     const detail = document.querySelector('.detail')
     if(detail.classList.contains('d-off')){
         openThis(detail)
-        content.classList.add('show__detail')
         detailHandler()
     }
 }
