2ab30dd3d8c0c6caba5f6eb6d646389e70cc7a7d
godsadeser <ducmanh0801@gmail.com>
2024-03-01 16:25:45 +0700

    import css files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './style/style.css'
+import './style/header.css'
+import './style/nav.css'
+import './style/menu.css'
+import './style/content.css'
+import './style/item.css'
+import './style/detail.css'
+import './style/dialog.css'
 import renderLayout from './utils/render'
 
 function ScreenController(){
