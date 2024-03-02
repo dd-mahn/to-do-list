fc8bcb0f21291250533d28b07a7aae7afc3278a5
godsadeser <ducmanh0801@gmail.com>
2024-03-02 17:11:43 +0700

    Add edit dialog and confirm dialog

--- src/component/Layout/createDialogs.js
+++ src/component/Layout/createDialogs.js
@@ -3,6 +3,7 @@ import createDivWithClass from "../../utils/common/createDivWithClass"
 import projectContainer from '../projectContainer'
 import {getCurrentState} from '../../utils/state'
 
+// Project add dialog
 export function createProjectDialog() {
     const dialog = document.createElement('dialog')
     dialog.id = 'project__add-dialog'
@@ -20,12 +21,13 @@ export function createProjectDialog() {
         </div>
     `
 
-    appendChildren(form, [createButtonDiv()])
+    appendChildren(form, [createAddButtonDiv()])
     dialog.appendChild(form)
 
     return dialog
 }
 
+// Item add dialog
 export function createItemDialog() {
     const dialog = document.createElement('dialog')
     dialog.id = 'item__add-dialog'
@@ -42,13 +44,9 @@ export function createItemDialog() {
     const todoAddForm = createTodoAddForm()
     const noteAddForm = createNoteAddForm()
 
-    appendChildren(dialog, [selectItemForm, todoAddForm, noteAddForm, createProjectSelect(), createButtonDiv()])
+    appendChildren(dialog, [selectItemForm, todoAddForm, noteAddForm, createProjectSelect(), createAddButtonDiv()])
 
     return dialog
-}
-
-export function createConfirmDialog() {
-    return document.createElement('dialog')
 }
 
 function createTodoAddForm() {
@@ -123,10 +121,96 @@ function createProjectSelect() {
     return projectSelectDiv
 }
 
-function createButtonDiv() {
+function createAddButtonDiv() {
     const buttonDiv = createDivWithClass('buttons')
     buttonDiv.innerHTML = `
         <button class="btn add__btn" formmethod="dialog">Create</button>
+        <button class="btn close__btn" formmethod="dialog">Cancel</button>`
+
+    return buttonDiv
+}
+
+// Confirm dialog
+export function createConfirmDialog() {
+    const dialog = document.createElement('dialog')
+    dialog.id = 'confirm__dialog'
+
+    const message = document.createElement('span')
+    message.innerHTML = `Are you sure?`
+
+    const buttonDiv = createDivWithClass('buttons')
+    buttonDiv.innerHTML = `
+        <button class="btn add__btn" formmethod="dialog">Yes</button>
+        <button class="btn close__btn" formmethod="dialog">No</button>`
+
+    appendChildren(dialog, [message, buttonDiv])
+
+    return dialog
+}
+
+// Item edit dialog
+export function createEditDialog(){
+    const dialog = document.createElement('dialog')
+    dialog.id = 'item__edit-dialog'
+
+    appendChildren(dialog, [createTodoEditForm(), createNoteEditForm(), createEditButtonDiv()])
+
+    return dialog
+}
+
+function createTodoEditForm(){
+    const todoEditForm = document.createElement('form')
+    todoEditForm.id = 'todo__edit-form'
+    todoEditForm.innerHTML = `
+        <div>
+            <label for="title">Title:</label>
+            <input required type="text" id="title" name="title" class="todo__title-input">
+        </div>
+        <div>
+            <label for="description">Description:</label>
+            <textarea required id="description" name="description" class="todo__des-input"></textarea>
+        </div>
+        <div>
+            <label for="startDate">Start date:</label>
+            <input required type="date" name="startDate" id="startDate" class="todo__start-input">
+        </div>
+        <div>
+            <label for="dueDate">Due date:</label>
+            <input required type="date" name="dueDate" id="dueDate" class="todo__due-input">
+        </div>
+        <div>
+            <label for="priority">Priority:</label>
+            <select name="priority" id="priority" class="todo__priority-input">
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>`
+
+    return todoEditForm
+}
+
+function createNoteEditForm(){
+    const noteEditForm = document.createElement('form')
+    noteEditForm.id = 'note__edit-form'
+    noteEditForm.classList.add('d-off')
+    noteEditForm.innerHTML = `
+        <div>
+            <label for="title">Title:</label>
+            <input required type="text" id="title" name="title" class="note__title-input">
+        </div>
+        <div>
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" class="note__des-input"></textarea>
+        </div>`
+
+    return noteEditForm
+}
+
+function createEditButtonDiv() {
+    const buttonDiv = createDivWithClass('buttons')
+    buttonDiv.innerHTML = `
+        <button class="btn add__btn" formmethod="dialog">Save</button>
         <button class="btn close__btn" formmethod="dialog">Cancel</button>`
 
     return buttonDiv
