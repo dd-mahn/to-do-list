628893139ec54dea9ccff876b4a926c38181dd40
godsadeser <ducmanh0801@gmail.com>
2024-03-05 00:57:40 +0700

    add search result obj

---
+++ src/component/Default Project/searchResult.js
@@ -0,0 +1,8 @@
+import project from "../project";
+
+export default function searchResult(){
+    const searchResultObj = Object.create(project())
+    searchResultObj.changeValue('Search Result', 'Where you can see your search results!')
+    return searchResultObj
+}
+
