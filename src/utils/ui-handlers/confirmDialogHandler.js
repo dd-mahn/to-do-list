5dae265a1f09ddc7001b952dfb2a7a5000af6bd0
godsadeser <ducmanh0801@gmail.com>
2024-03-04 19:52:13 +0700

    add confirm deleteProjectHandler

--- src/utils/ui-handlers/confirmDialogHandler.js
+++ src/utils/ui-handlers/confirmDialogHandler.js
@@ -1,5 +1,7 @@
+import inboxObj from "../../component/Default Project/inbox"
 import createUndoBox from "../../component/Layout/createUndoBox"
 import renderLayout from "../render"
+import { setCurrentState } from "../state"
 import { undoDeleteHandler } from "./undoHandler"
 
 let undoBoxQueue = [] // Queue to store undoBox elements
@@ -13,6 +15,32 @@ export function deleteConfirmHandler(project, index){
         const deleteItem = project.getItem(index)
         project.deleteItem(index)
         confirmDialog.close('deleted')
+        renderLayout()
+        setTimeout(() => {
+            const content = document.querySelector('.content')
+            const undoBox = createUndoBox('1 item deleted')
+            content.appendChild(undoBox)
+            undoDeleteHandler(project, deleteItem, undoBox) // Pass undoBox to undoDeleteHandler
+            addToQueue(undoBox) // Add undoBox to the queue
+            checkUndoBoxQueue() // Check the queue for handling undoBox elements
+        }, 500)
+    })
+
+    noBtn.addEventListener('click', () => {
+        confirmDialog.close('cancelled')
+    })
+}
+
+export function deleteProjectConfirmHandler(project, index){
+    const confirmDialog = document.getElementById('confirm__dialog')
+    const yesBtn = confirmDialog.querySelector('.add__btn')
+    const noBtn = confirmDialog.querySelector('.close__btn')
+
+    yesBtn.addEventListener('click', () => {
+        const deleteItem = project.getItem(index)
+        project.deleteItem(index)
+        confirmDialog.close('deleted')
+        setCurrentState(inboxObj)
         renderLayout()
         setTimeout(() => {
             const content = document.querySelector('.content')
