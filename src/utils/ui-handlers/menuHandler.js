5b79b9114f701e5e779f160bb1f461f9f3378668
godsadeser <ducmanh0801@gmail.com>
2024-03-04 16:38:42 +0700

    add openProjectList when open projects

--- src/utils/ui-handlers/menuHandler.js
+++ src/utils/ui-handlers/menuHandler.js
@@ -55,6 +55,7 @@ export default function menuHandler(){
             setCurrentState(targetProject)
             renderLayout()
             openMenu()
+            openProjectList()
         })
     })
 
