e5bfede293af486b186084b58bae39c92c27d62c
godsadeser <ducmanh0801@gmail.com>
2024-03-06 01:46:14 +0700

    add animation

--- src/utils/ui-handlers/projectDialogHandler.js
+++ src/utils/ui-handlers/projectDialogHandler.js
@@ -1,6 +1,7 @@
 // For projectDialogHandler
 import project from "../../component/project"
 import projectContainerObj from "../../component/projectContainer"
+import executeWithAnimation from "../common/executeWithAnimation"
 import renderLayout from "../render"
 import { setCurrentState } from "../state"
 import { openMenu, openProjectList } from "./menuHandler"
@@ -14,31 +15,37 @@ export default function projectDialogHandler() {
     const closeBtn = projectForm.querySelector('.close__btn')
 
     closeBtn.addEventListener('click', () => {
-        projectForm.removeAttribute('novalidate')
-        projectDialog.close('canceled')
-        projectDialog.classList.remove('df')
-        projectForm.setAttribute('novalidate', 'true')
+        executeWithAnimation(projectDialog, () => {
+            projectForm.removeAttribute('novalidate')
+            projectDialog.close('canceled')
+            projectDialog.classList.remove('df')
+            projectForm.setAttribute('novalidate', 'true')
+        })
+        
     })
 
     addBtn.addEventListener('click', () => {
         const allProjects = projectContainerObj.getAllItem()
         const nameValue = nameInput.value.trim()
         const desValue = desInput.value.trim()
+        
         if (nameValue && desValue) {
-            const existingProject = allProjects.find(prj => prj.getValue().title === nameValue)
-            const newProject = project()
-            if (existingProject) {
-                newProject.changeValue(nameValue + '1', desValue)
-            } else {
-                newProject.changeValue(nameValue, desValue)
-            }
-            projectContainerObj.addItem(newProject)
-            setCurrentState(newProject)
-            renderLayout()
-            openMenu()
-            openProjectList()
+            executeWithAnimation(projectDialog, () => {
+                const existingProject = allProjects.find(prj => prj.getValue().title === nameValue)
+                const newProject = project()
+                if (existingProject) {
+                    newProject.changeValue(nameValue + '1', desValue)
+                } else {
+                    newProject.changeValue(nameValue, desValue)
+                }
+                projectContainerObj.addItem(newProject)
+                setCurrentState(newProject)
+                renderLayout()
+                openMenu()
+                openProjectList()
+            }) 
         } else {
             console.log('Empty input')
-        }
+        }            
     })
 }
