9b65d1f2a6f0c06e96e4a2f91fc299b2abd71d7d
godsadeser <ducmanh0801@gmail.com>
2024-03-07 18:13:13 +0700

    Remove unused createDetail import

--- src/component/Layout/createLayout.js
+++ src/component/Layout/createLayout.js
@@ -2,7 +2,6 @@ import header from './createHeader'
 import navBar from './createNavigation'
 import menu from './createMenu'
 import createContent from './createContent'
-import createDetail from './createDetail'
 import {createProjectDialog, createItemDialog, createConfirmDialog, createEditDialog, createMoveDialog} from './createDialogs'
 import appendChildren from '../../utils/common/appendChildren'
 
@@ -13,7 +12,6 @@ export default function createLayout(obj){
     container.classList.add('container')
 
     const content = createContent(obj)
-    // const detail = createDetail(obj)
     const projectDialog = createProjectDialog()
     const itemDialog = createItemDialog()
     const editDialog = createEditDialog()
